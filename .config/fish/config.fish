679138f9e84a33a8a4e3bfb6e6d2946729c1eb63
Max Bossing <info@maxbossing.de>
2023-08-09 16:33:09 +0200

    Merge fish config into one file

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -4,13 +4,90 @@
 #██║  ██║██║   ██║   ██║   ██╔══╝  ██║██║     ██╔══╝  ╚════██║ Author: Max
 #██████╔╝╚██████╔╝   ██║   ██║     ██║███████╗███████╗███████║ Github: github.com/maxbossing/dotfiles
 #╚═════╝  ╚═════╝    ╚═╝   ╚═╝     ╚═╝╚══════╝╚══════╝╚══════╝ Config: Fish - Main Config
-# This is just here to do the last bit
 
+# === EXPORTS ===
+# Variable exports
+
+# Remove fish greeting
+set fish_greeting
+
+# Set terminal/visual editor to nvim
+set EDITOR 'nvim'
+set VISUAL 'nvim'
+
+
+
+# === GPG ===
+# This is used so the pgp keys on my Yubikey can be used for ssh etc
+
+# Start ssh-agent
+eval (ssh-agent -c)
+
+# Start gpg agent
+gpg-agent --enable-ssh-support
+
+# Set ssh auth sock to gpg agent
+set SSH_AUTH_SOCK (gpgconf --list-dirs agent-ssh-socket)
+
+# I don't know what this does but we need it
+set GPG_TTY (tty)
+
+# connect agents
+gpg-connect-agent updatestartuptty /bye >/dev/null
+
+# Switch gpg to backup keys
+# Main key
+alias switchto1='gpg-connect-agent "20576321" "learn --force" /bye'
+# Backup Key
+alias switchto2='gpg-connect-agent "20576243" "learn --force" /bye'
+
+
+
+# === ALIAS ===
+# Aliases
+
+# Map vim to nvim
+alias vim='nvim'
+
+# exa > ls
+alias ls='exa -al --group-directories-first --icons --color=always'
+
+# My dotfiles are managed through a git bare repo in my $HOME
+alias config='/usr/bin/git --git-dir=$HOME/.cfg --work-tree=$HOME'
+
+
+
+#=== COLORS ===
+# Fish colors
+
+# Default Color
+set fish_color_normal brpurple
+
+# Suggestion Color
+set fish_color_autosuggestion '#8114b8'
+
+# Command Color
+set fish_color_command brpurple
+
+# Error Color
+set fish_color_error '#D11313'
+
+# Parameter Color
+set fish_color_param brpurple
+
+
+
+#=== TMUX ===
+# Attach to tmux session
 if status is-interactive
 and not set -q TMUX
     exec tmux -f ~/tmux.conf new-session -A -s terminal
 end
 
-clear  # Just clear
+#=== CLEANUP ===
 
-starship init fish | source # start starship
+# clear outputs
+clear 
+
+# int startship
+starship init fish | source  
